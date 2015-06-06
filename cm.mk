ce13417c28d0ae77a8337e651eaf5af9b2cf0190
Marcel112 <vmellie@hotmail.com>
2015-06-06 10:53:53 +0200

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,10 +1,11 @@
-$(call inherit-product, device/samsung/klte/full_klte.mk)
+
+$(call inherit-product, device/samsung/hltespr/full_klte.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Validus stuff.
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := klte
-PRODUCT_NAME := cm_klte
+PRODUCT_NAME := validus_klte
